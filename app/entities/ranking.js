474233846dac904ee9e81b86843a6f5efc59846f
pedrobuenoxs <pedrohbueno02@gmail.com>
2023-01-06 11:10:01 -0300

    add logic to check if the user is registered and return sorted data do user profile request

--- app/entities/ranking.js
+++ app/entities/ranking.js
@@ -17,6 +17,10 @@ module.exports = class Ranking {
   }
   async updateScore(date) {
     try {
+      const isRegistered = await this.user.isRegistered;
+      if (!isRegistered) {
+        throw new Error("Você não está registrado!");
+      }
       let user = await this.user.updateScore(date);
       return user.score;
     } catch (error) {
@@ -38,8 +42,9 @@ module.exports = class Ranking {
       const user = await this.user.getUser();
       let msg = `Histórico de ${user.name}:\n`;
       const data = user.data;
-      data.forEach((day) => {
-        msg += `${day.date} - ${day.score}/100\n`;
+      const sortedData = data.sort((a, b) => b.date - a.date);
+      sortedData.forEach((day, index) => {
+        msg += `${day.date} - ${index}/100\n`;
       });
       return msg;
     } catch (error) {

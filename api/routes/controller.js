9e6709a13d5b417d21112dcd7d992fcd0dbdf1d7
pedrobuenoxs <pedrohbueno02@gmail.com>
2023-01-06 11:09:10 -0300

    creating update user score controller

--- api/routes/controller.js
+++ api/routes/controller.js
@@ -56,4 +56,22 @@ const createUser = async (req, res) => {
   }
 };
 
+const updateUserScore = async (req, res) => {
+  try {
+    const { userID, score, data } = req.body;
+    if (!userID || !score || !data) throw new Error("Missing params");
+    const update = await repo.updateScore({ userID, score, data });
+    const user = {
+      id: update.userID,
+      name: update.name,
+      imgUrl: update.imgUrl,
+      score: update.score,
+      data: update.data,
+    };
+    res.send(user);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 module.exports = { getData, getUser, createUser };

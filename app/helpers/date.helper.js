f1551c2ef71861c582dfb4459a4afc9d29f8389b
pedrobuenoxs <pedrohbueno02@gmail.com>
2023-01-16 14:44:31 -0300

    fix

--- app/helpers/date.helper.js
+++ app/helpers/date.helper.js
@@ -2,6 +2,7 @@ const yesterdayDate = () => new Date(Date.now() - 86400000 - 10800000);
 
 const dateInBrazil = (_date) => {
   const threeHoursAgo = () => new Date(Date.now() - 10800000);
+  console.log("Date on production: ", threeHoursAgo());
   const date = _date || threeHoursAgo();
   const stringDate = date.toLocaleString("pt-BR");
   const arrayDate = stringDate.split(" ");

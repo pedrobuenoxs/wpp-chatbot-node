63aeb2059c80b78b3facef913aae79a5f86fd8c0
pedrobuenoxs <pedrohbueno02@gmail.com>
2023-01-18 15:37:33 -0300

    add script to restart Dyno

--- restartDynos.js
+++ restartDynos.js
@@ -2,9 +2,8 @@ const fetch = require("node-fetch");
 require("dotenv").config();
 const HEROKU_API_TOKEN = process.env.HEROKU_API_TOKEN;
 const HEROKU_APP_ID = process.env.HEROKU_APP_ID;
-console.log(HEROKU_APP_ID);
 
-console.log("Restarting dynos");
+console.log("Restarting dynos", Date.now());
 const getResponse = async () => {
   const response = await fetch(
     `https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`,
